4ecde119b3c2d6582d503ad3fe3ccac7c73d09bc
M Sowmya <sowmya20111.com>
2025-07-05 07:49:07 +0530

    Java_fundamentals

---
+++ java_fundamentals/src/java_fundamentals/Question14.java
@@ -0,0 +1,13 @@
+package java_fundamentals;
+import java.util.*;
+public class Question14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
